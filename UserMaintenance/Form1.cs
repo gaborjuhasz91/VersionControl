4f07bc3a60b58bcfe5014439761cbd39e207500a
gabor juhasz <gabor.juhasz@icellmobilsoft.hu>
2019-12-08 19:54:32 +0100

    FullName bevezetése

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -18,8 +18,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            lblLastName.Text = Resource1.LastName; // label1
-            lblFirstName.Text = Resource1.FirstName; // label2
+            lblFullName.Text = Resource1.FullName; // label1
             btnAdd.Text = Resource1.Add; // button1
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -35,8 +34,8 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+                FullName = txtFullName.Text,
+               
             };
             users.Add(u);
         }
