6ccb7ba4e1f7ef4adc232b170a7ae6441844a558
gabor juhasz <gabor.juhasz@icellmobilsoft.hu>
2019-12-08 19:48:40 +0100

    Add button

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,22 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource1.LastName; // label1
             lblFirstName.Text = Resource1.FirstName; // label2
             btnAdd.Text = Resource1.Add; // button1
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
